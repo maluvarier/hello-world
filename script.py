b396478b5468905eb1b7df6af3ce1792c53e3cf6
malavika.v <malavika.v@tataelxsi.co.in>
2019-05-21 12:27:49 +0530

    1st change dev1

--- script.py
+++ script.py
@@ -1,12 +1,1 @@
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
-print("x has changed to 2")
-print("x has changed to 2")
-print("Developer1_from PC")
-print("Developer2_from PC")
-print("Developer1_from PC_new change")
-print("Developer1_from PC_current")
-print("Developer2_from PC")
-print("Developer2_from PC_tuesday")
+print("Dev1 - change 1")
